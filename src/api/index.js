b93ccf8f374beeb691769d66652ef5552a4ac3cb
Emilie <emiliempark@gmail.com>
2020-02-10 18:18:29 +1300

    Display search result

--- src/api/index.js
+++ src/api/index.js
@@ -4,24 +4,22 @@ export const apiCall = axios.create({
     baseURL: 'https://pokeapi.co/api/v2/pokemon/'
 });
 
-export function getPokemon(ID_OR_NAME) {
-    apiCall.get(`${ID_OR_NAME}`).then(
-        (res) => {
-            console.log('get pokemon', res);
+export async function getPokemon(ID_OR_NAME) {
+    try{
+        let res = await apiCall.get(`${ID_OR_NAME}`);
+        return res.data;
+    }
+    catch(err) {
+        if(err.response){
+            // request was made, server responded with a status code
+            console.log('data', err.response.data);
+            console.log('status', err.response.status);
+            console.log('headers', err.response.headers);
+        }else{
+            console.log();
         }
-    ).catch(
-        (err) => {
-            if(err.response){
-                // request was made, server responded with a status code
-                console.log('data', err.response.data);
-                console.log('status', err.response.status);
-                console.log('headers', err.response.headers);
-            }else{
-                console.log();
-            }
-            console.log(err.config);
-            // when 404 print 'not found' message
-        }
-    )
+        console.log(err.config);
+        // when 404 print 'not found' message
+    }
 
 }
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,22 +2,41 @@ import React, { useState } from 'react'
 import { getPokemon } from '../../api';
 
 export default function Search() {
-    function searchPokemon(e){
+    let [keyword, setKeyword] = useState('');
+    let [result, setResult] = useState();
+    async function searchPokemon(e){
         e.preventDefault();
-        console.log('search...', e.target.value, keyword);
-        getPokemon(keyword);
+        console.log('search...', keyword);
+        if(keyword.length !== 0){
+            let result = await getPokemon(keyword);
+            console.log('test', result);
+            if(result !== undefined){
+                setResult(result);
+            }
+        }    
     }
+    
+
     function handleKeyword(e){
         setKeyword(e.target.value);
     }
-    let [keyword, setKeyword] = useState();
+
+    function addToTeam(){
+        console.log('add to team');
+    }
 
     return (
         <div>
+            <p>Search Pokemon</p>
             <form>
                 <input type="text" placeholder="ID or Name" onChange={(e) => { handleKeyword(e) }}/>
                 <input type="submit" onClick={(e)=> {searchPokemon(e)}} />
             </form>
+
+            <p>Result</p>
+            {result !== undefined && (
+                <p>{result.name} {result.id} <button onClick={addToTeam}> Add to Team</button></p>
+            )}
         </div>
     )
 }
